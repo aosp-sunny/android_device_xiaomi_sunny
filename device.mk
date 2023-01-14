8bc2438b9a0b2d3728cc2818a0c31d1f1230b131
blurzyy <selster02@gmail.com>
2023-01-14 14:47:54 +0800

    Revert "sm6150-common: Don't inherit updatable_apex.mk"
    - We're not building lineage or pe anymore.
    This reverts commit eec13c1391cc7b5dd2e04ac0cd6adcf93796123f.

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/sunny/sunny-vendor.mk)
 
