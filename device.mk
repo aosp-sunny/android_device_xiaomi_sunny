bb8f70e3c80e2102c85ce8244c245aa675842cc9
Cyber Knight <cyberknight755@gmail.com>
2023-02-26 05:12:04 +0800

    sunny: Build DisplayFeatures
    - Add DisplayFeatures to PRODUCT_PACKAGES.
    - Address DisplayFeatures related SEPolicy rules.
    - Define sysfs_displayfeatures as a domain.
    - Allow system_app to read/write vendor_sysfs_graphics
    - Allow system_app to search vendor_sysfs_graphics
    - Setup permission of nodes relevant to DisplayFeatures at boot.
    - Setup DC Dim and HBM nodes as RRO for DisplayFeatures.
    
    Change-Id: Ie907a07350c0c12ac334a956acbf697cf997089d
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -208,6 +208,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml
 
+# DisplayFeatures
+PRODUCT_PACKAGES += \
+    DisplayFeatures
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0.vendor \
@@ -382,6 +386,7 @@ PRODUCT_COPY_FILES += \
 # Overlay
 PRODUCT_PACKAGES += \
     CarrierConfigSunny \
+    DisplayFeaturesSunny \
     KProfilesSunny \
     FrameworksSunny \
     SettingsProviderSunny \
